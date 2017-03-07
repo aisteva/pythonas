c04af9e747d6dafa16b93c3c5427c7f3da61db77
Aiste <aiste.vaznelyte@gmail.com>
2017-03-07 21:03:15 +0200

    Create table in sql file

--- DB/sqlexample.py
+++ DB/sqlexample.py
@@ -3,5 +3,14 @@ db = sql.connect(host="mysql-uosis.mif", port=3306, user="aiva2297", passwd="Ryt
 cursor = db.cursor()
 cursor.execute("SELECT version()")
 result = cursor.fetchone()
+
+
+# Create table as per requirement
+sql = """CREATE TABLE PEOPLE (
+         FIRST_NAME  CHAR(20),
+         LAST_NAME  CHAR(20),
+         AGE INT  )"""
+
+cursor.execute(sql)
 print(result)
 db.close()
