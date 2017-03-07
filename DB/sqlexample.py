c6a78244a67a23256335e8ff0f65e1ed2c5f37f9
Aiste <aiste.vaznelyte@gmail.com>
2017-03-07 21:07:54 +0200

    Insert data in table in sql file

--- DB/sqlexample.py
+++ DB/sqlexample.py
@@ -6,11 +6,23 @@ result = cursor.fetchone()
 
 
 # Create table as per requirement
-sql = """CREATE TABLE PEOPLE (
-         FIRST_NAME  CHAR(20),
-         LAST_NAME  CHAR(20),
-         AGE INT  )"""
+#sql = """CREATE TABLE PEOPLE (
+ #        FIRST_NAME  CHAR(20),
+ #        LAST_NAME  CHAR(20),
+ #        AGE INT  )"""
+		 
+# Prepare SQL query to INSERT a record into the database.
+sql = """INSERT INTO PEOPLE(FIRST_NAME,
+         LAST_NAME, AGE)
+         VALUES ('Jonas', 'Joninis', 25)"""
+try:
+   # Execute the SQL command
+   cursor.execute(sql)
+   # Commit your changes in the database
+   db.commit()
+except:
+   # Rollback in case there is any error
+   db.rollback()
 
-cursor.execute(sql)
 print(result)
 db.close()
