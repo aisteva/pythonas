d6b5b3321f279a8581c1ef03d9802f8d25f6bc79
Aiste <aiste.vaznelyte@gmail.com>
2017-03-19 12:33:44 +0200

    Added noddy.c example and compilation in setup.py

--- Ispletimas_iterpimas/setup.py
+++ Ispletimas_iterpimas/setup.py
@@ -3,3 +3,5 @@ from distutils.core import Extension
 MOD = "spam"
 module = Extension(MOD, sources =["python.c"])
 setup(name = MOD, ext_modules = [module])
+setup(name="noddy",
+      ext_modules=[Extension("noddy", ["noddy.c"])])
