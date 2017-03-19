22f0aa82081678a9a62f709263ae7c72a6e6e3f9
Aiste <aiste.vaznelyte@gmail.com>
2017-03-19 16:06:54 +0200

    Added new noddy2.c file from exampe, update in setup.pu

--- Ispletimas_iterpimas/setup.py
+++ Ispletimas_iterpimas/setup.py
@@ -3,5 +3,8 @@ from distutils.core import Extension
 MOD = "spam"
 module = Extension(MOD, sources =["python.c"])
 setup(name = MOD, ext_modules = [module])
-setup(name="noddy",
-      ext_modules=[Extension("noddy", ["noddy.c"])])
+setup(name="noddy", version="1.0",
+      ext_modules=[
+         Extension("noddy", ["noddy.c"]),
+         Extension("noddy2", ["noddy2.c"]),
+         ])
