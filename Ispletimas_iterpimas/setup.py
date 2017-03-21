235203aab5e4391e74dcc52720433e74299f34b9
Aiste <aiste.vaznelyte@gmail.com>
2017-03-21 17:16:30 +0200

    added imple.c

--- Ispletimas_iterpimas/setup.py
+++ Ispletimas_iterpimas/setup.py
@@ -6,4 +6,5 @@ setup(name = MOD, ext_modules = [module])
 setup(name="dataType", version="1.0",
       ext_modules=[
          Extension("dataType", ["dataType.c"]),
+		 Extension("imple.c", ["imple.c"])
          ])
