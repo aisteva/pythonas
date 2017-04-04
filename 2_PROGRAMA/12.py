4e5cab32012a5899cfb9a3a80d60a5efd922f34e
Aiste <aiste.vaznelyte@gmail.com>
2017-04-04 14:42:57 +0300

    Pataisyta 12.py neapibreztinis integralas

--- 2_PROGRAMA/12.py
+++ 2_PROGRAMA/12.py
@@ -1,15 +1,16 @@
 #12. Pasirinktos funkcijos apibreztini ir neapibreztini integralus
 
 from scipy.integrate import quad
-import numpy
+import numpy as np
 
 def f(x):
     return x**2
 
 ats1, e1 = quad(f, 0, 2)
-ats2, e2 = quad(f, -numpy.inf, +numpy.inf)
+p = np.poly1d([1,0,0])
+ats2 = np.polyint(p)
 
 
 print("Apibreztinis integralas:  ", ats1)
-print("Neapibreztinis integralas:  ", ats2)
+print("Neapibreztinis integralas:  \n", ats2)
 
