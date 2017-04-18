15222bfd3c5c502c82679a288c58533f9e9c2a80
root <root@unix-os-lectures>
2017-04-18 13:08:41 +0300

    select for show data

--- DB/sqlexample.py
+++ DB/sqlexample.py
@@ -6,7 +6,7 @@
 
 import pymysql as sql
 
-db = sql.connect(host='localhost', port=3306, user='root', passwd='', db='python')
+db = sql.connect(host='localhost', port=3306, user='root', passwd='123456', db='python')
 
 cursor = db.cursor()
 
@@ -14,11 +14,13 @@ name = str(input('Enter name: '))
 lastname = str(input('Enter last name: '))
 age = int(input('Enter age: '))
 
+sql2 = """SELECT * FROM PEOPLE"""
 sql = """CREATE TABLE IF NOT EXISTS PEOPLE (
     FIRST_NAME CHAR(20),
     LAST_NAME CHAR(20),
     AGE INT)"""
 sql1 = """INSERT INTO PEOPLE (FIRST_NAME, LAST_NAME, AGE) VALUES ('%s', '%s', %d)""" % (name, lastname, age)
+
 	
 try:
 # Execute the SQL commands
@@ -26,8 +28,16 @@ try:
     print("Table created")
     cursor.execute(sql1)
     print("Data inserted")
+    cursor.execute(sql2)
+    result = cursor.fetchall()
+    for row in results:
+        name = row[0]
+        last = row[1]
+        age = row[2]
 # Commit your changes in the database
     db.commit()
+
+    print("name = %s, last = %s, age = %d" % (name, last, age))
 except:
    # Rollback in case there is any error
     db.rollback()
