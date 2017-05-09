60b473020005f41e23a90353ed18d88577e1541b
root <root@unix-os-lectures>
2017-05-09 13:07:55 +0300

    veikia polls, modifikuotas urls.py

--- psl/uzd5/urls.py
+++ psl/uzd5/urls.py
@@ -1,8 +1,1 @@
-from django.conf.urls import include, url
-from django.contrib import admin
-from polls import views
 
-urlpatterns = [
-    url(r'^polls/', include('polls.urls')), 
-    url(r'^admin/', admin.site.urls), 
-]
