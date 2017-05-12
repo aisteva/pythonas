f6802215bd42d4df3bc58815e861b8cb18f80d0b
root <root@unix-os-lectures>
2017-05-12 17:54:03 +0300

    Veikia details

--- psl/uzd5/polls/urls.py
+++ psl/uzd5/polls/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [url(r'^$', views.index, name='index'),
     #"ex:"/polls/5/"
-    url(r'^(?P<aktas_id>[0n9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<aktas_id>[0-9]+)/$', views.detail, name='detail'),
     #"ex:"/polls/5/results/"
-    url(r'^(?P<aktas_id>[0n9]+)/results/$', views.results, name='results'),]
+    url(r'^(?P<aktas_id>[0-9]+)/results/$', views.results, name='results'),]
