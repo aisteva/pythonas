668f6d23ad4f5d7883d096dbbdbdbef6d17fe299
Aiste <aiste.vaznelyte@gmail.com>
2017-05-12 16:33:01 +0300

    Pridėti vaizdai karkasas3 tema

--- psl/uzd5/polls/urls.py
+++ psl/uzd5/polls/urls.py
@@ -1,4 +1,8 @@
 from django.conf.urls import url
 from . import views
 
-urlpatterns = [url(r'^$', views.index, name='index'),]
+urlpatterns = [url(r'^$', views.index, name='index'),
+    #"ex:"/polls/5/"
+    url(r'^(?P<aktas_id>[0n9]+)/$', views.detail, name='detail'),
+    #"ex:"/polls/5/results/"
+    url(r'^(?P<aktas_id>[0n9]+)/results/$', views.results, name='results'),]
--- psl/uzd5/polls/views.py
+++ psl/uzd5/polls/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Aktas
+from django.template import loader
+from django.http import Http404
 
 def index(request):
-    return HttpResponse("Pirmas kažkam skirtas puslapis - polls.")
+    latest_aktas_list = Aktas.objects.order_by('-pub_date')[:5]
+	template = loader.get_template('polls/index.html')
+    #context = { 'latest_aktas_list':latest_aktas_list, }
+	#output = ','.join([a.pavadinimas for a in latest_aktas_list])
+    return render(request, 'polls/index.html', context)
+	
+def detail(request, aktas_id):
+    return HttpResponse("Matomas klausimas, kurio id {0}.".format(aktas_id))
+	
+	
+def results(request, aktas_id):
+    response ="Klausimo {0} rezultatai."
+    return HttpResponse(response.format(aktas_id))
 # Create your views here.
