d94d56915d94978127005bfe4bf918be4745faba
Aiste <aiste.vaznelyte@gmail.com>
2017-05-14 18:54:58 +0300

    pridėtas pdf generavimas

--- psl/uzd5/polls/urls.py
+++ psl/uzd5/polls/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 from . import views
-from polls.views import AktasCreate
+from polls.views import AktasCreate, GeneratePDF
+
 
 urlpatterns = [url(r'^$', views.index, name='index'),
     #"ex:"/polls/5/"
-    url(r'^(?P<aktas_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'a/(?P<aktas_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'p/(?P<aktas_id>[0-9]+)/$', views.detailPDF, name='detailPDF'),
     #"ex:"/polls/add/"
     url(r'aktas/add/$', views.AktasCreate.as_view(), name='aktas-add'),]
     #url(r'^(?P<aktas_id>[0-9]+)/results/$', views.results, name='results'),]
--- psl/uzd5/polls/views.py
+++ psl/uzd5/polls/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Aktas
-from django.template import loader
+from django.template import loader, Context
 from django.http import Http404
 from django.db.models import Sum
 from django.views.generic.edit import CreateView
-
+from django.views.generic import View
+from xhtml2pdf import pisa 
+from django.template.loader import get_template
+from polls.utils import render_to_pdf #created in step 4
 
 class AktasCreate(CreateView):
         model = Aktas
@@ -18,12 +21,29 @@ def index(request):
     context = { 'latest_aktas_list':latest_aktas_list, }
 #output = ','.join([a.pavadinimas for a in latest_aktas_list])
     return render(request, 'polls/index.html', context)
+	
 
+
+
+class GeneratePDF(View):
+    def get(self, request, *args, **kwargs):
+        latest_aktas_list = Aktas.objects.order_by('-data')[:5]
+        template = loader.get_template('polls/index.html')
+        context = { 'latest_aktas_list':latest_aktas_list, }
+        html = template.render(context)
+        return HttpResponse(html)
 
 def detail(request, aktas_id):
     aktas = get_object_or_404(Aktas, pk = aktas_id)
     total = aktas.vertybe.all().aggregate(Sum('suma'))
     return render(request, 'polls/detail.html', {'aktas': aktas})
+	
+	
+def detailPDF(request, aktas_id):
+    aktas = get_object_or_404(Aktas, pk = aktas_id)
+    total = aktas.vertybe.all().aggregate(Sum('suma'))
+    pdf = render_to_pdf('polls/detail.html', {'aktas': aktas})
+    return HttpResponse(pdf, content_type = 'application/pdf')
 
 	
 
